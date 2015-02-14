c05b32dab612309f42784f1cb766e42414c0bedd
Toby Mackenzie <public@tobymackenzie.com>
2015-02-13 22:58:29 -0500

    pieces/skeleton/meta: don't both echo and pass `$display` = `true` to `wp_title()`

--- pieces/skeleton/meta.php
+++ pieces/skeleton/meta.php
@@ -1,6 +1,6 @@
 <meta charset="<?php bloginfo('charset'); ?>">
 <meta content="initial-scale=1,width=device-width" name="viewport" />
-<title><?php echo wp_title($tjmThemeHelper->data->get('pageTitleSeparator'), true, $tjmThemeHelper->data->get('pageTitleLocation')); ?></title>
+<title><?php wp_title($tjmThemeHelper->data->get('pageTitleSeparator'), true, $tjmThemeHelper->data->get('pageTitleLocation')); ?></title>
 
 <link rel="pingback" href="<?php echo bloginfo('pingback_url'); ?>" />
 <link rel="profile" href="http://gmpg.org/xfn/11" />
